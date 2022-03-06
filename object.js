c4245c2339a73476c905d68e7a4021a6842cbd81
stefanie18 <89417960+stefanie18@users.noreply.github.com>
2022-03-06 13:17:33 -0600

    completed

---
+++ object.js
@@ -0,0 +1,18 @@
+class info {
+    constructor(
+        name,
+        lastName,
+        email,
+        PIDM,
+        school,
+        gradeClassification,
+    )
+    {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.PIDM = PIDM;
+        this.school = school;
+        this.gradeClassification = gradeClassification;
+    };
+}
